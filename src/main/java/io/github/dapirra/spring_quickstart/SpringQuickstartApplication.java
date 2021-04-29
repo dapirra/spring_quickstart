042c4217e0eb8d8607dde874d55ce2fe99dc1c0f
dapirra <35818038+dapirra@users.noreply.github.com>
2021-04-29 13:45:46 -0400

    Downloaded initial project files

---
+++ src/main/java/io/github/dapirra/spring_quickstart/SpringQuickstartApplication.java
@@ -0,0 +1,13 @@
+package io.github.dapirra.spring_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dapirra/spring_quickstart/SpringQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dapirra.spring_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
